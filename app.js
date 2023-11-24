ac35102d50e76b5d79e760738e2c2f13a5af84a7
Nasir Uddin <43319931+nasircli@users.noreply.github.com>
2023-11-24 12:04:36 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,105 @@
-const express = require('express')
-const path = require("path");
-const app = express()
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { URL, URLSearchParams } = require('url');
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-// #############################################################################
-// Logs all request paths and method
-app.use(function (req, res, next) {
-  res.set('x-timestamp', Date.now())
-  res.set('x-powered-by', 'cyclic.sh')
-  console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
-  next();
-});
+async function getTagsFromUrl(url, tagSelector) {
+    try {
+        const headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        };
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
+        const response = await axios.get(url, { headers });
+        const $ = cheerio.load(response.data);
+
+        const tags = [];
+        $(tagSelector).each((index, element) => {
+            tags.push($(element).text().trim());
+        });
+
+        return tags;
+    } catch (error) {
+        if (error.response) {
+            const { status } = error.response;
+            if (status === 403) {
+                console.log(`Error 403: Access forbidden for URL: ${url}`);
+            } else if (status === 404) {
+                console.log(`Error 404: URL not found: ${url}`);
+            } else {
+                console.log(`HTTP Error: ${status} - ${error.message}`);
+            }
+        } else {
+            console.log(`Error: ${error.message}`);
+        }
+        return null;
+    }
 }
-app.use(express.static('public', options))
 
-// #############################################################################
-// Catch all handler for all other request.
-app.use('*', (req,res) => {
-  res.json({
-      at: new Date().toISOString(),
-      method: req.method,
-      hostname: req.hostname,
-      ip: req.ip,
-      query: req.query,
-      headers: req.headers,
-      cookies: req.cookies,
-      params: req.params
-    })
-    .end()
-})
+async function getCrawledData(mainInput, tagSelector) {
+    try {
+        const parsedUrl = new URL(mainInput);
+        const mainUrl = parsedUrl.href;
 
-module.exports = app
+        const headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        };
+
+        const response = await axios.get(mainUrl, { headers });
+        const $ = cheerio.load(response.data);
+
+        const mainTags = await getTagsFromUrl(mainUrl, tagSelector);
+
+        const uniqueTags = new Set(mainTags);
+
+        if (mainTags) {
+            console.log(`Best 3 Tags from Each Images (1st Page) - ${mainUrl}:`);
+            console.log(mainTags.join(', '));
+            console.log('-'.repeat(50));
+        }
+
+        const links = [];
+        $('body.new-resource-list .filter-tags-row .tag-slider--list li a, body.new-resource-list .no-results--popular .tag-slider--list li a').each((index, element) => {
+            const link = $(element).attr('href');
+            links.push(new URL(link, mainUrl).href);
+        });
+
+        for (const link of links) {
+            const tags = await getTagsFromUrl(link, tagSelector);
+            if (tags) {
+                tags.forEach(tag => uniqueTags.add(tag));
+                await sleep(1000);  // Add a 1-second delay between requests to avoid being blocked
+            }
+        }
+
+        console.log('Tags from All Slider Tags:');
+        console.log([...uniqueTags].join(', '));
+        console.log('-'.repeat(50));
+
+        displayAllTags([...uniqueTags]);
+
+    } catch (error) {
+        if (error.response) {
+            const { status } = error.response;
+            if (status === 403) {
+                console.log(`Error 403: Access forbidden for URL: ${mainUrl}`);
+            } else if (status === 404) {
+                console.log(`Error 404: URL not found: ${mainUrl}`);
+            } else {
+                console.log(`HTTP Error: ${status} - ${error.message}`);
+            }
+        } else {
+            console.log(`Error: ${error.message}`);
+        }
+    }
+}
+
+function displayAllTags(uniqueTags) {
+    const allUniqueTags = [...new Set(uniqueTags)];
+    console.log('All Unique Tags (after removing duplicates):');
+    console.log(allUniqueTags.join(', '));
+    console.log('-'.repeat(50));
+}
+
+// Prompt the user for a main link or keyword to crawl
+const mainInput = prompt('Enter the main link or keyword to crawl: ');
+getCrawledData(mainInput, '.showcase .showcase__item.showcase__item--buttons .showcase__thumbnail .tags-container ul.tags>li>.tag-item');
